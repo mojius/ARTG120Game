c13a832774e8be1bb0f246dd14f69bd8cd520236
Iyassu Allen <iyassu.allen@gmail.com>
2023-05-28 18:05:17 -0700

    Added Wall Jump + Slide
    Can wall jump if CAN_WALL_JUMP is true

--- Player.gd
+++ Player.gd
@@ -9,8 +9,16 @@ const JUMP_VELOCITY = 200
 
 var walk = 0
 
+var CAN_JUMP = true
+var NUM_JUMPS = 0
+var MAX_NUM_JUMPS = 1
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
+# Wall jump variables
+const WALL_SLIDE_ACCEL = 7.5
+const MAX_WALL_SLIDE_SPEED = 120
+var CAN_WALL_JUMP = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,18 +26,27 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 		
 	
-	walk = WALK_FORCE * (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
-	
-	
+	walk = WALK_FORCE * (Input.get_action_strength("Right") - Input.get_action_strength("Left")) #"ui_right","ui_left"
+	if is_on_floor():
+		CAN_JUMP = true
+		NUM_JUMPS = MAX_NUM_JUMPS
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -JUMP_VELOCITY
+	if Input.is_action_just_pressed("Jump"): #ui_accept
+		if CAN_JUMP == true && NUM_JUMPS > 0:
+			velocity.y = -JUMP_VELOCITY
+			NUM_JUMPS = NUM_JUMPS - 1
+			if is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Right"): #"ui_right"
+				velocity.x = -WALK_MAX_SPEED
+				NUM_JUMPS = NUM_JUMPS - 1
+			elif is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Left"): #"ui_left"
+				velocity.x = WALK_MAX_SPEED
+				NUM_JUMPS = NUM_JUMPS - 1
 
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_just_released("Jump"): #ui_accept
 		jump_cut()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right") #"ui_left", "ui_right"
 	
 	if direction > 0 or direction < 0:
 		$PlayerSprite.scale.x = direction
@@ -42,7 +59,19 @@ func _physics_process(delta):
 	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
 	
 	move_and_slide()
+	
+	#Wall Slide/Jump Detection
+	if is_on_wall() && CAN_WALL_JUMP && (Input.is_action_pressed("Right") || Input.is_action_pressed("Left")): #"ui_left", "ui_right"
+		CAN_JUMP = true
+		NUM_JUMPS = MAX_NUM_JUMPS
+		if velocity.y >= 0:
+			velocity.y = min(velocity.y - WALL_SLIDE_ACCEL, MAX_WALL_SLIDE_SPEED)
+		else:
+			velocity.y += GRAVITY * delta
+	else:
+		velocity.y += GRAVITY * delta
 
 func jump_cut():
 	if velocity.y < -100:
 		velocity.y = -100
+
