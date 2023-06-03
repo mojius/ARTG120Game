c8ea947526340b41326017f8f1107b9e101d1ffe
Iyassu Allen <iyassu.allen@gmail.com>
2023-06-02 19:15:09 -0700

    Rock Smash Downward dash
    mapped to x

--- Player.gd
+++ Player.gd
@@ -16,55 +16,56 @@ var MAX_NUM_JUMPS = 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
 # Wall jump variables
-#const WALL_SLIDE_ACCEL = 2.5
-#const MAX_WALL_SLIDE_SPEED = 10
 const WALL_SLIDE_SPEED = 20
 var CAN_WALL_JUMP = true
 
 #Float variables
 const FLOAT_DECEL = 40
-#const MAX_FLOAT_DECEL = 35
 var isFloating = false
+
+#Rock Smash Variables
+const dashSpeed = 700
+const dashLength = 0.1
+
+@onready var dash = $Dash
 
 # BUGS #
 # Fast wall climb
-# If you mess around a wall while floating it sometimes keeps you in a glide state even if your not pressing ctrl, pressing ctrl undoes it
+# If you mess around a wall while floating it sometimes keeps you in a 
+#....glide state even if your not pressing ctrl, pressing ctrl undoes it
 
 func _physics_process(delta):
 	# Add the gravity.
-	#print_debug(isFloating)
+	print_debug(isFloating)
 	if not is_on_floor() && not is_on_wall_only():
-		velocity.y += GRAVITY * delta
+		velocity.y += GRAVITY * delta 
 		NUM_JUMPS = NUM_JUMPS - 1
-		
+	if Input.is_action_just_pressed("Dash") && not is_on_floor():
+			dash.startDash(dashLength)
+			if dash.isDashing():
+				velocity.y = dashSpeed
 	if not is_on_floor():
 		if Input.is_action_pressed("Float"):
 			$PlayerSprite.play("Glide")
 			isFloating = true
 		else:
 			isFloating = false
-		#if Input.is_action_just_released("Float"):
-			#isFloating = false
 		if isFloating && velocity.y >= 0:
-			velocity.y = FLOAT_DECEL #min(velocity.y + FLOAT_DECEL, MAX_FLOAT_DECEL)
+			velocity.y = FLOAT_DECEL 
 	
-	walk = WALK_FORCE * (Input.get_action_strength("Right") - Input.get_action_strength("Left")) #"ui_right","ui_left"
+	walk = WALK_FORCE * (Input.get_action_strength("Right") - Input.get_action_strength("Left")) 
 	if is_on_floor():
 		CAN_JUMP = true
 		NUM_JUMPS = MAX_NUM_JUMPS
 	# Handle Jump.
-	if Input.is_action_just_pressed("Jump"): #ui_accept
+	if Input.is_action_just_pressed("Jump"): 
 		if CAN_JUMP == true && NUM_JUMPS > 0:
 			velocity.y = -JUMP_VELOCITY
 			NUM_JUMPS = NUM_JUMPS - 1
-			if is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Right"): #"ui_right"
-				#NUM_JUMPS = MAX_NUM_JUMPS
+			if is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Right"):
 				velocity.x = -WALK_MAX_SPEED
-				#NUM_JUMPS = NUM_JUMPS - 1
-			elif is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Left"): #"ui_left"
-				#NUM_JUMPS = MAX_NUM_JUMPS
+			elif is_on_wall() && CAN_WALL_JUMP && Input.is_action_pressed("Left"):
 				velocity.x = WALK_MAX_SPEED
-				#NUM_JUMPS = NUM_JUMPS - 1
 	
 	if velocity.y < 0 and Input.is_action_just_pressed("Jump"):
 		$PlayerSprite.play("Jump")
@@ -73,11 +74,11 @@ func _physics_process(delta):
 		$PlayerSprite/GPUParticles2D.emitting = false
 		
 	
-	if Input.is_action_just_released("Jump"): #ui_accept
+	if Input.is_action_just_released("Jump"): 
 		jump_cut()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("Left", "Right") #"ui_left", "ui_right"
+	var direction = Input.get_axis("Left", "Right") 
 	
 	if direction > 0 or direction < 0:
 		$PlayerSprite.scale.x = direction
@@ -106,12 +107,11 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	#Wall Slide/Jump Detection
-	if is_on_wall_only() && CAN_WALL_JUMP && (Input.is_action_pressed("Right") || Input.is_action_pressed("Left")): #"ui_left", "ui_right"
+	if is_on_wall_only() && CAN_WALL_JUMP && (Input.is_action_pressed("Right") || Input.is_action_pressed("Left")): 
 		CAN_JUMP = true
 		NUM_JUMPS = MAX_NUM_JUMPS
 		$PlayerSprite.play("WallGrab")
 		if velocity.y >= 0:
-			#velocity.y = min(velocity.y - WALL_SLIDE_ACCEL, MAX_WALL_SLIDE_SPEED)
 			velocity.y = WALL_SLIDE_SPEED
 		else:
 			velocity.y += GRAVITY * delta
