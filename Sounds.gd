1b92e56339ba1466641435e862d82ff17e50ddaa
Luca Triola <34149179+Slimentist@users.noreply.github.com>
2023-06-02 20:01:28 -0700

    Dash And Sound Fix

--- Sounds.gd
+++ Sounds.gd
@@ -7,7 +7,7 @@ var playedSound = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if $"../PlayerSprite".is_playing() and $"../PlayerSprite".animation == "Run" and $"../PlayerSprite".frame == walkSoundFrameStart and playedSound == false:
-		$AudioStreamPlayer2D.play()
+		$AudioStreamPlayer.play()
 		playedSound = true
 	elif playedSound == true and $"../PlayerSprite".animation == "Run" and $"../PlayerSprite".frame != walkSoundFrameStart:
 		playedSound =false
