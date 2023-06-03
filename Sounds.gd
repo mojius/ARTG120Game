dfbca2673d0e29b7242a19e999a96b4531992f24
Luca Triola <34149179+Slimentist@users.noreply.github.com>
2023-06-02 18:22:38 -0700

    Walk Sound Fix

--- Sounds.gd
+++ Sounds.gd
@@ -2,7 +2,12 @@ extends Node
 
 @export var walkSoundFrameStart = 2
 
+
+var playedSound = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if $"../PlayerSprite".is_playing() and $"../PlayerSprite".animation == "Run" and $"../PlayerSprite".frame == walkSoundFrameStart:
+func _process(_delta):
+	if $"../PlayerSprite".is_playing() and $"../PlayerSprite".animation == "Run" and $"../PlayerSprite".frame == walkSoundFrameStart and playedSound == false:
 		$AudioStreamPlayer2D.play()
+		playedSound = true
+	elif playedSound == true and $"../PlayerSprite".animation == "Run" and $"../PlayerSprite".frame != walkSoundFrameStart:
+		playedSound =false
